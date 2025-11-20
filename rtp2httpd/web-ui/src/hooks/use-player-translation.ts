49d2675a80cacb555bd7234d1267cc580181929a
Shawleevip <Shawleevip@users.noreply.github.com>
2025-11-21 02:20:42 +0800

    update 2025-11-21 02:20:42

--- rtp2httpd/web-ui/src/hooks/use-player-translation.ts
+++
@@ -1,8 +0,0 @@
-import { useCallback } from "react";
-import type { Locale } from "../lib/locale";
-import type { TranslationKey } from "../i18n/player";
-import { translate } from "../i18n/player";
-
-export function usePlayerTranslation(locale: Locale) {
-  return useCallback((key: TranslationKey) => translate(locale, key), [locale]);
-}
